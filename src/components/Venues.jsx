fb0df7e8c7c74a12712586b13f7eabc6627160f9
Pete Naish <pete@nai.sh>
2019-06-16 11:32:36 +0100

    add map placeholder

--- src/components/Venues.jsx
+++ src/components/Venues.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { map } from 'lodash';
+import mapPlaceholder from '../map-placeholder.png';
 
 export default ({
   venues
@@ -9,7 +10,7 @@ export default ({
       map(venues, (venue) => (
         <li key={venue.name}>
           <h2>{venue.name}</h2>
-          <img alt={venue.name} src={`https://i.pravatar.cc/150?u=${venue.name}`} />
+          <img alt={venue.name} src={mapPlaceholder} />
         </li>
       ))
     }
