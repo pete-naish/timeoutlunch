2bd388bae6b216378a500da6e9c7ddab7869a6cb
Pete Naish <pete@nai.sh>
2019-06-16 12:02:13 +0100

    update list of venues when selected users change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
-import { map } from 'lodash';
+import { map, filter, every, isEmpty, difference, includes, find } from 'lodash';
 import './App.css';
-import users from './config/users';
-import venues from './config/venues';
+import normalizedUsers from './config/users';
+import normalizedVenues from './config/venues';
 import Users from './components/Users';
 import Venues from './components/Venues';
 
 class App extends Component {
   state = {
-    users: users,
-    groupCanVisit: [],
+    users: normalizedUsers,
+    venues: normalizedVenues,
+  }
+
+  updateVenues = () => {
+    const { users } = this.state;
+    const selectedUsers = filter(users, 'isSelected');
+
+    const eachUserCanVisit = map(selectedUsers, (user) => {
+      const canVisit = filter(normalizedVenues, (venue) => {
+        const canEat = !isEmpty(difference(venue.food, user.wont_eat));
+        const canDrink = !isEmpty(filter(venue.drinks, (drink) => includes(user.drinks, drink)));
+
+        return canEat && canDrink;
+      });
+
+      return {
+        name: user.name,
+        canVisit,
+      };
+    });
+
+    this.setState({
+      venues: filter(normalizedVenues, (venue) =>
+        every(eachUserCanVisit, (user) =>
+          find(user.canVisit, ['name', venue.name])
+        )
+      )
+    });
   }
 
   onUserClick = (user) => {
@@ -22,17 +49,17 @@ class App extends Component {
 
     this.setState({
       users: map(users, (stateUser) => stateUser.name === user.name ? updatedUser : stateUser),
-    });
+    }, this.updateVenues);
   }
 
   render() {
     const {
       users,
-      groupCanVisit,
+      venues,
     } = this.state;
 
     return (
-      <div className="App">
+      <div>
         <section>
           <h1>Who's going?</h1>
           <Users onClick={this.onUserClick} users={users} />
