a426abb39fa9adf63f68b0bb9bdbe9031dc686dc
Pete Naish <pete@nai.sh>
2019-06-16 10:52:09 +0100

    add basic styling and handle user click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { map } from 'lodash';
 import './App.css';
 import users from './config/users';
 import venues from './config/venues';
 import Users from './components/Users';
 import Venues from './components/Venues';
 
-function App() {
-  return (
-    <div className="App">
-      <section>
-        <h1>Who's going?</h1>
-        <Users users={users} />
-      </section>
-      <section>
-        <h1>Where to?</h1>
-        <Venues venues={venues} />
-      </section>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: users,
+    groupCanVisit: [],
+  }
+
+  onUserClick = (user) => {
+    const { users } = this.state;
+
+    const updatedUser = {
+      ...user,
+      isSelected: !user.isSelected,
+    };
+
+    this.setState({
+      users: map(users, (stateUser) => stateUser.name === user.name ? updatedUser : stateUser),
+    });
+  }
+
+  render() {
+    const {
+      users,
+      groupCanVisit,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <section>
+          <h1>Who's going?</h1>
+          <Users onClick={this.onUserClick} users={users} />
+        </section>
+        <section>
+          <h1>Where to?</h1>
+          <Venues venues={venues} />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { map } from 'lodash';
 
 export default ({
-  users
+  users,
+  onClick,
 }) => (
-  <ul>
+  <ul className="users">
     {
       map(users, (user) => (
-        <li key={user.name}>
+        <li onClick={() => onClick(user)} className={ user.isSelected ? 'isSelected' : '' } key={user.name}>
           <h2>{user.name}</h2>
           <img alt={user.name} src={`https://i.pravatar.cc/150?u=${user.name}`} />
         </li>
