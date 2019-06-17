5ab4b01999edb94fae66d8f73a00f0f4e1358933
Pete Naish <pete@nai.sh>
2019-06-17 17:03:14 +0100

    add tests for venue filtering

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,53 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import Users from './components/Users';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import allUsersFixture from './fixtures/all-users-fixture';
+import allVenuesFixture from './fixtures/all-venues-fixture';
+import firstUserSelectedFixture from './fixtures/first-user-selected-fixture';
+import threeUsersSelectedFixture from './fixtures/three-users-selected-fixture';
+import threeUsersFilteredVenuesFixture from './fixtures/three-users-filtered-venues-fixture';
+import allUsersSelectedFixture from './fixtures/all-users-selected-fixture';
+import allUsersFilteredVenuesFixture from './fixtures/all-users-filtered-venues-fixture';
+
+describe('selecting one user for lunch', () => {
+  it('should filter out the correct venues', () => {
+    const wrapper = shallow(<App />);
+    const UsersComponent = shallow(<Users onClick={wrapper.instance().onUserClick} users={allUsersFixture} />);
+
+    UsersComponent.find('#john-davis').simulate('click');
+    expect(wrapper.state('users')).toEqual(firstUserSelectedFixture)
+    expect(wrapper.state('venues')).toEqual(allVenuesFixture);
+  });
+});
+
+describe('selecting three users for lunch', () => {
+  it('should filter out the correct venues', () => {
+    const wrapper = shallow(<App />);
+    const UsersComponent = shallow(<Users onClick={wrapper.instance().onUserClick} users={allUsersFixture} />);
+
+    UsersComponent.find('#john-davis').simulate('click');
+    UsersComponent.find('#gary-jones').simulate('click');
+    UsersComponent.find('#robert-webb').simulate('click');
+    expect(wrapper.state('users')).toEqual(threeUsersSelectedFixture)
+    expect(wrapper.state('venues')).toEqual(threeUsersFilteredVenuesFixture);
+  });
+});
+
+describe('selecting all users for lunch', () => {
+  it('should filter out the correct venues', () => {
+    const wrapper = shallow(<App />);
+    const UsersComponent = shallow(<Users onClick={wrapper.instance().onUserClick} users={allUsersFixture} />);
+
+    UsersComponent.find('#john-davis').simulate('click');
+    UsersComponent.find('#gary-jones').simulate('click');
+    UsersComponent.find('#robert-webb').simulate('click');
+    UsersComponent.find('#gavin-coulson').simulate('click');
+    UsersComponent.find('#alan-allen').simulate('click');
+    UsersComponent.find('#bobby-robson').simulate('click');
+    UsersComponent.find('#david-lang').simulate('click');
+    expect(wrapper.state('users')).toEqual(allUsersSelectedFixture)
+    expect(wrapper.state('venues')).toEqual(allUsersFilteredVenuesFixture);
+  });
 });
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { map } from 'lodash';
+import { map, kebabCase } from 'lodash';
 
 export default ({
   users,
@@ -8,7 +8,7 @@ export default ({
   <ul className="users">
     {
       map(users, (user) => (
-        <li onClick={() => onClick(user)} className={ user.isSelected ? 'isSelected' : '' } key={user.name}>
+        <li id={kebabCase(user.name)} onClick={() => onClick(user)} className={ user.isSelected ? 'isSelected' : '' } key={user.name}>
           <h3>{user.name}</h3>
           <img alt={user.name} src={`https://i.pravatar.cc/150?u=${user.name}`} />
         </li>
